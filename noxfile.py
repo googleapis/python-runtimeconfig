608dee8a4cc9f822d9f592ca35ec1b79373685f0
chalmer lowe <chalmerlowe@google.com>
2025-11-14 11:11:00 -0500

    fix: Correct indentation in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -174,7 +174,10 @@ def install_unittest_dependencies(session, *constraints):
     ["python", "upb", "cpp"],
 )
 def unit(session, protobuf_implementation):
-        if protobuf_implementation == "cpp" and session.python in ("3.11", "3.12", "3.13", "3.14"):        session.skip("cpp implementation is not supported in python 3.11+")
+    # Install all test dependencies, then install this package in-place.
+
+    if protobuf_implementation == "cpp" and session.python in ("3.11", "3.12", "3.13", "3.14"):
+        session.skip("cpp implementation is not supported in python 3.11+")
 
     constraints_path = str(
         CURRENT_DIRECTORY / "testing" / f"constraints-{session.python}.txt"
